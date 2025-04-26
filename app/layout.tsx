c19dd9c685db72544be307f0cd60f2606727f2e7
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-04-26 18:16:53 +0300

    Configure ThemeProvider (light/dark theme)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "@/theme";
+import InitColorSchemeScript from "@mui/material/InitColorSchemeScript";
+import ModeSwitch from "@/components/ModeSwitch";
 
 export const metadata: Metadata = {
   title: "DOiT MVP",
@@ -13,7 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>
+        <InitColorSchemeScript attribute="class" />
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <ModeSwitch />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Copyright from "@/components/Copyright";
+
 export default function Home() {
   return (
-    <div>
-      <main></main>
-      <footer></footer>
-    </div>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Configure ThemeProvider (light/dark theme)
+        </Typography>
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
