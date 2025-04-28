0e9920804d714db2b9b6747061d0356e1297491c
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-04-28 13:23:12 +0300

    Deleting a resource
    resource will not be really updated on the server but it will be faked as if.

---
+++ components/DialogDelete.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useDeletePostMutation } from "@/lib/features/posts/postsSlice";
+import { useRouter } from "next/navigation";
+
+interface DialogProps {
+  id: number;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function DialogDelete({
+  open,
+  setOpen,
+  id,
+}: Readonly<DialogProps>) {
+  const [isDeleting, setIsDeleting] = React.useState(false);
+  const router = useRouter();
+  const [deletePost] = useDeletePostMutation();
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDelete = async () => {
+    try {
+      setIsDeleting(true);
+      await deletePost(id).unwrap();
+      setIsDeleting(false);
+      handleClose();
+      router.push("/posts");
+    } catch (error) {
+      console.error("Error deleting post:", error);
+      alert("Error deleting the post");
+    }
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        {"Are you sure you want to delete this post?"}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          Are you sure you want to delete this post?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+        <Button loading={isDeleting} onClick={handleDelete} autoFocus>
+          Agree
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
--- components/PostDetailsCard.tsx
+++ components/PostDetailsCard.tsx
@@ -10,38 +10,53 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Post } from "@/lib/types/post";
 import { Button } from "@mui/material";
+import DialogDelete from "./DialogDelete";
 
 interface PostCardProps {
   post: Post;
 }
 
 export default function PostDetailsCard({ post }: Readonly<PostCardProps>) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
   return (
-    <Card sx={{ width: "100%" }}>
-      <CardHeader
-        avatar={<Avatar aria-label="recipe">{post.userId}</Avatar>}
-        title={post.title}
-        subheader={post.userId}
-      />
-      <CardContent>
-        <Typography variant="body2" sx={{ color: "text.secondary" }}>
-          {post.body}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button variant="contained" color="error" startIcon={<DeleteIcon />}>
-          ВИДАЛИТИ
-        </Button>
-        <Button
-          variant="outlined"
-          component={Link}
-          href={`/posts`}
-          aria-label="back"
-          startIcon={<ArrowBackIcon />}
-        >
-          ДО СПИСКУ
-        </Button>
-      </CardActions>
-    </Card>
+    <>
+      <Card sx={{ width: "100%" }}>
+        <CardHeader
+          avatar={<Avatar aria-label="recipe">{post.userId}</Avatar>}
+          title={post.title}
+          subheader={post.userId}
+        />
+        <CardContent>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            {post.body}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            color="error"
+            startIcon={<DeleteIcon />}
+            onClick={handleClickOpen}
+          >
+            ВИДАЛИТИ
+          </Button>
+          <Button
+            variant="outlined"
+            component={Link}
+            href={`/posts`}
+            aria-label="back"
+            startIcon={<ArrowBackIcon />}
+          >
+            ДО СПИСКУ
+          </Button>
+        </CardActions>
+      </Card>
+      {open && <DialogDelete open={open} setOpen={setOpen} id={post.id} />}
+    </>
   );
 }
